1605716770b3141fdd75af5e8fdc3ace7b2a8470
Thongchai Kitiyanantawong <mypumz@gmail.com>
2019-02-22 02:26:31 +0700

    Updates

---
+++ precache-manifest.b5b6c9f135a6dfe6a60de136bccac227.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "9c593912696201a380a8",
+    "url": "/react-todo-list-app/static/js/runtime~main.9c593912.js"
+  },
+  {
+    "revision": "164864d8aa7d9d75679d",
+    "url": "/react-todo-list-app/static/js/main.164864d8.chunk.js"
+  },
+  {
+    "revision": "ebf1408a23b1a4a8d564",
+    "url": "/react-todo-list-app/static/js/2.ebf1408a.chunk.js"
+  },
+  {
+    "revision": "177771e2bb0a068238b488249a244770",
+    "url": "/react-todo-list-app/index.html"
+  }
+];
