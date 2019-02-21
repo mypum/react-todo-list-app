f57aa2b5115d1a4aa2da932b85fd322a5735f2bc
Thongchai Kitiyanantawong <mypumz@gmail.com>
2019-02-22 01:24:21 +0700

    Add default store for show example to do items

--- src/features/Todo/context.js
+++ src/features/Todo/context.js
@@ -2,7 +2,38 @@ import React from 'react'
 
 const TodoStore = React.createContext({
   tab: 'all',
-  todos: [],
+  todos: [
+    {
+      id: 12221,
+      content: 'Book Flights to Paris',
+      status: 'incomplete',
+    },
+    {
+      id: 234234,
+      content: 'Watch Interstellar',
+      status: 'incomplete',
+    },
+    {
+      id: 345345,
+      content: 'Send presentation to Frank',
+      status: 'incomplete',
+    },
+    {
+      id: 234623,
+      content: 'Diary of a Wimpy Kid',
+      status: 'incomplete',
+    },
+    {
+      id: 2357806,
+      content: 'เอารถไปล้าง',
+      status: 'completed',
+    },
+    {
+      id: 444378,
+      content: 'จ่ายบิลค่าโทรศัพท์',
+      status: 'completed',
+    },
+  ],
 })
 
 export default TodoStore
